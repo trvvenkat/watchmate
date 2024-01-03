560f1f3e7dccd4d19ea3d7ba335509200e06d8f7
Venkatesh T R <venkateshramkumar01@gmail.com>
2024-01-03 14:49:02 +0530

    uploaded

---
+++ watchlist_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from watchlist_app.views import movies_list, movie_details
+
+urlpatterns = [
+    path("list/", movies_list, name="movie-list"),
+    path("<int:pk>", movie_details, name="movie-details")
+]
