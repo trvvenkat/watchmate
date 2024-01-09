fa97b1bed728e0e95d98a2674b5c866fbdba8336
Venkatesh <venkateshramkumar01@gmail.com>
2024-01-09 13:07:31 +0530

    model serializers

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,36 +1,24 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
-# validators
-def name_check(value):
-    if len(value) < 2:
-        raise serializers.ValidationError("Title length is too short")
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_check])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
-    
-    def create(self, validated_data):
-        
-        return Movie.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        
-        return instance
+# Model Serializers
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+
+        model = Movie
+        fields = "__all__"  # this with include all the fields
+        # fields = ["id", "name"] # include only the fileds that we mention here
+        # exclude = ["id"] # fields that we want to exclude
+
+
     
     # field validator
-    # def validate_name(self, value):
-    #     if len(value) < 2:
-    #         raise serializers.ValidationError("Title length is too short")
-    #     else:
-    #         return value
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Title length is too short")
+        else:
+            return value
         
 
     #object validator
@@ -39,4 +27,45 @@ class MovieSerializer(serializers.Serializer):
             raise serializers.ValidationError("Title and description cannot be the same")
         else:
             return data
+
+
+
+# validators
+# def name_check(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError("Title length is too short")
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_check])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+    
+#     def create(self, validated_data):
+        
+#         return Movie.objects.create(**validated_data)
+    
+#     def update(self, instance, validated_data):
+        
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+        
+#         return instance
+    
+#     # field validator
+#     # def validate_name(self, value):
+#     #     if len(value) < 2:
+#     #         raise serializers.ValidationError("Title length is too short")
+#     #     else:
+#     #         return value
+        
+
+#     #object validator
+#     def validate(self, data):
+#         if data["name"] == data["description"]:
+#             raise serializers.ValidationError("Title and description cannot be the same")
+#         else:
+#             return data
     
